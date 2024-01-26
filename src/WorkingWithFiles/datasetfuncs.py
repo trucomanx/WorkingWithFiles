4b8b52c4da2018c78706943bd9d754fba8d59325
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-01-26 17:06:51 -0300

    mejorando

--- src/WorkingWithFiles/datasetfuncs.py
+++ src/WorkingWithFiles/datasetfuncs.py
@@ -5,7 +5,7 @@ import csv
 import numpy as np
 
 
-def generate_csv_file_from_dir_structure(base_dir,format_list,csv_path):
+def generate_csv_file_from_dir_structure(base_dir,format_list,csv_path,header = ['filename', 'label']):
     label_list = [ name for name in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, name)) ]
     file_list_list=[];
     #print('label_list:',label_list)
@@ -14,11 +14,10 @@ def generate_csv_file_from_dir_structure(base_dir,format_list,csv_path):
         tmp_path = os.path.join(base_dir,label);
         file_list = wf.get_all_files_in_dir(tmp_path,formats_search=format_list,is_relative=True);
         file_list_list.append(file_list);
-
+    
     with open(csv_path, 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
-
-        header = ['filename', 'label'];
+    
         writer.writerow(header)
         
         Nel=[];
@@ -28,7 +27,11 @@ def generate_csv_file_from_dir_structure(base_dir,format_list,csv_path):
         N=np.max(Nel);
         M=len(label_list);
         
+        out=[]
         for n in range(N):
             for m in range(M):
                 if(n<Nel[m]):
-                    writer.writerow([os.path.join(label_list[m],file_list_list[m][n]), label_list[m]])
+                    item=[os.path.join(label_list[m],file_list_list[m][n]), label_list[m]];
+                    writer.writerow(item);
+                    out.append(item);
+    return out;
